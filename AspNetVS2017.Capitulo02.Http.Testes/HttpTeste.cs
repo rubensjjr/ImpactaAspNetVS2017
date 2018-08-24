291a2f18ec03f0d80f43d3cc533cb04478fa530a
rubensjjr <rubens.junior@outlook.com.br>
2018-08-23 22:40:30 -0300

    MVC-23/08

--- AspNetVS2017.Capitulo02.Http.Testes/HttpTeste.cs
+++ AspNetVS2017.Capitulo02.Http.Testes/HttpTeste.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Net;
+using System.IO;
+using System.Text;
+using System.Web;
 
 namespace AspNetVS2017.Capitulo02.Http.Testes
 {
@@ -15,8 +18,70 @@ namespace AspNetVS2017.Capitulo02.Http.Testes
             request.Method = "GET";
             request.UserAgent = "Visual Studio";
             request.Date = DateTime.Now;
+            request.Headers.Add("Authorization", "ushkdhjkfhsfhusdfhuidhuisdfhuisfdh");
 
-            
+            Console.WriteLine(GetRequestToString(request));
+
+            Console.WriteLine(new string('-',100));
+
+            Console.WriteLine("Query string:" + request.RequestUri.Query);
+
+            Console.WriteLine(new string('-', 100));
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            Console.WriteLine(GetResponseToString(response));
+
+        }
+
+        [TestMethod]
+        public void RequestPostTeste()
+        {
+            var request = (HttpWebRequest)WebRequest.Create("http://httpbin.org/post");
+
+            request.Method = "POST";
+
+            var dados = "nome=Vitor&cpf=2354654&endereco=R.tal";
+            var bytes = new ASCIIEncoding().GetBytes(HttpUtility.UrlEncode(dados));
+
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = bytes.Length;
+            request.GetRequestStream().Write(bytes, 0, bytes.Length);
+
+            Console.WriteLine(GetRequestToString(request, dados));
+
+            Console.WriteLine(new string('-', 100));
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            Console.WriteLine(GetResponseToString(response));
+        }
+
+        private string GetResponseToString(HttpWebResponse response)
+        {
+            var statusLine = $"HTTP/{response.ProtocolVersion} {(int)response.StatusCode}{response.StatusDescription}";
+            var reader = new StreamReader(response.GetResponseStream());
+
+            return statusLine + Environment.NewLine + GetHeaders(response.Headers) + Environment.NewLine + reader.ReadToEnd();
+        }
+
+        private string GetRequestToString(HttpWebRequest request, string dados = null)
+        {
+            var requestLine = $"{request.Method} {request.RequestUri} HTTP/{request.ProtocolVersion}";
+
+            return requestLine + Environment.NewLine + GetHeaders(request.Headers) + Environment.NewLine + dados;
+        }
+
+        private string GetHeaders(WebHeaderCollection header)
+        {
+            var headers = string.Empty;
+            foreach (var key in header.AllKeys)
+            {
+                headers += $"{key}: {header[key]}" + Environment.NewLine;
+            }
+
+
+            return headers;
         }
     }
 }
---
+++ AspNetVS2017.Capitulo03.Portfolio/Models/ContatoViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetVS2017.Capitulo03.Portfolio.Models
+{
+    public class ContatoViewModel
+    {   
+        [Required] // faz a validação dos campos em java
+        public string Nome { get; set; }
+
+        [Required]
+        [EmailAddress] // valida o email na tela
+        public string Email { get; set; }
+
+        [Required]
+        public string Mensagem { get; set;}
+
+    }
+}
